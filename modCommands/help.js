dc074e9c2d7d701de767480ed7ae354fa290299d
Raymond van Someren <raymondvansomeren@live.nl>
2020-12-16 22:38:17 +0100

    help commands added

---
+++ modCommands/help.js
@@ -0,0 +1,44 @@
+const { modPrefix } = require('../config.json');
+
+module.exports =
+{
+    name: 'help',
+    description: 'List all of my commands or info about a specific command.',
+    aliases: ['commands'],
+    usage: '[command name (not required)]',
+    cooldown: 3,
+    execute(bot, message, args)
+    {
+        const data = [];
+        const { modCommands } = message.client;
+
+        if (!args.length)
+        {
+            data.push('Here\'s a list of all my commands:');
+            for (const cmd of modCommands.map(command => command.name))
+                data.push(`:white_small_square: **${cmd}**`);
+            data.push(`\nYou can send \`${modPrefix}help [command name]\` to get info on a specific command!`);
+
+            return message.channel.send(data, { split: true });
+        }
+
+        const name = args[0].toLowerCase();
+        const command = modCommands.get(name) || modCommands.find(c => c.aliases && c.aliases.includes(name));
+
+        if (!command)
+            return message.reply('that\'s not a valid command!');
+
+        data.push(`**Name:** ${command.name}`);
+
+        if (command.aliases)
+            data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+        if (command.description)
+            data.push(`**Description:** ${command.description}`);
+        if (command.usage)
+            data.push(`**Usage:** ${modPrefix}${command.name} ${command.usage}`);
+
+        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+        message.channel.send(data, { split: true });
+    },
+};
