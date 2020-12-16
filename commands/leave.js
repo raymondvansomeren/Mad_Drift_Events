bc007d4e2c4e6c156b04ae67b360256e0c91e262
Raymond van Someren <raymondvansomeren@live.nl>
2020-12-16 23:01:41 +0100

    leave now works

---
+++ commands/leave.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+module.exports = {
+    name: 'leave',
+    description: 'Leave a role',
+    aliases: ['leaverole'],
+    usage: '[rolename]',
+    cooldown: 5,
+    execute(bot, message, args)
+    {
+        const data = fs.readFileSync('./roles.json');
+        const json = JSON.parse(data);
+        if (json.guild.find(element => element.id === message.guild.id) === undefined)
+        {
+            json.guild.push({ 'id': message.guild.id, 'roles': new Array() });
+            fs.writeFileSync('./roles.json', JSON.stringify(json));
+            return message.channel.send('There don\'t seem to be any roles to leave.');
+        }
+        const roles = JSON.parse(data).guild.find(element => element.id === message.guild.id).roles;
+
+        if (roles === undefined || roles.length === 0)
+            return message.channel.send('There don\'t seem to be any roles to leave.');
+
+        const role = message.guild.roles.cache.find(r =>
+        {
+            const jsonRole = roles.find(element => element.value === args[0]);
+            if (jsonRole === undefined)
+                return undefined;
+            return r.name === jsonRole.name;
+        });
+
+        if (role === undefined)
+            return message.channel.send('No role with that name found. Use `e!joinable` to see all joinable roles.');
+        message.member.roles.remove(role);
+        message.channel.send(`Relieved you from the role \`${role.name}\`.`);
+    },
+};
