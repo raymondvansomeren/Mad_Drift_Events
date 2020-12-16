31a59e642397052f5e9b637fa428f877c5ee1b0f
Raymond van Someren <raymondvansomeren@live.nl>
2020-12-16 22:11:45 +0100

    joinable now works

---
+++ commands/joinable.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+    name: 'joinable',
+    description: 'Lists joinable roles',
+    aliases: ['roles'],
+    usage: '',
+    cooldown: 5,
+    execute(bot, message, args)
+    {
+        // TODO
+        const data = fs.readFileSync('./roles.json');
+        const roles = JSON.parse(data).guild.find(element => element.id === message.guild.id).roles;
+
+        if (roles === undefined || roles.length === 0)
+            return message.channel.send('There don\'t seem to be any joinable roles.');
+
+        const embedConstruction = {
+            color: '#9D1731',
+            title: 'All joinable roles',
+            timestamp: new Date(),
+            footer:
+            {
+                icon_url: bot.user.avatarURL(),
+                text: '© raymond570#2966',
+            },
+        };
+
+        let reply = new Discord.MessageEmbed(embedConstruction);
+
+        for (let i = 0; i < roles.length; i++)
+        {
+            if (i % 21 === 0 && i !== 0)
+            {
+                message.channel.send(reply);
+                reply = new Discord.MessageEmbed(embedConstruction);
+            }
+            reply.addField(`${roles[i].name}`, `Use \`e!join ${roles[i].value}\``, true);
+        }
+
+        message.channel.send(reply);
+    },
+};
